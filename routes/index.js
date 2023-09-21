e3f3f957e5e66451bc162dfd7a78ab343368e59d
mahmoud-24 <mahmoud.ibrahim2411@gmail.com>
2023-09-22 01:07:47 +0300

    new deployment statge test

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Shift EV WebApp' });
+    res.render('index', { title: 'Shift EV WebApp, hello there' });
 });
 
 module.exports = router;
